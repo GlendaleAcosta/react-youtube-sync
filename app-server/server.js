2eb1ec8813ebee57f07f39e18b7abac4ec523521
Glendale Acosta <gglendale17@gmail.com>
2018-04-18 17:12:47 -0700

    Added lint and server

---
+++ app-server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.load({ path: '.env' });
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../build')))
+
+
+// Controllers
+
+
+
+// Routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+
+// Server
+app.listen(PORT, () => {
+  console.log(`App is up on port ${PORT}`);
+});
