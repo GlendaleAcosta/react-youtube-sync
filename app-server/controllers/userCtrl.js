fa6e48bbec028c74927f0d8a0a4b575824f16294
Glendale Acosta <gglendale17@gmail.com>
2018-04-18 23:42:33 -0700

    Implemented Sequelize and postgres for app

--- app-server/controllers/userCtrl.js
+++ app-server/controllers/userCtrl.js
@@ -1,24 +1,40 @@
-// const sequelize = require('../Sequelize');
-// const DataTypes = require('sequelize/lib/data-types');
-// const User = require('../models/User')(sequelize, DataTypes);
+const sequelize = require('../Sequelize');
+const DataTypes = require('sequelize/lib/data-types');
+const User = require('../models/User')(sequelize, DataTypes);
 
 exports.postSignUp = (req, res) => {
-  console.log('we are here');
   console.log(req.body);
-  // User
-  // .findOrCreate({ where: { email: req.body.email }, defaults: { password: req.body.password } })
-  // .spread((user, created) => {
-  //   if (created) {
-  //     return res.json({
-  //       user,
-  //     });
-  //   }
+  User
+  .findOrCreate({ where: { email: req.body.email }, defaults: {
+    password: req.body.password,
+    username: req.body.username
+  }})
+  .spread((user, created) => {
+    console.log(user);
+    if (created) {
+      return res.json({
+        user,
+        // token
+      });
+    }
     return res.json({
-      user: {
-        id: 'ihgrQz99',
-        email: 'gglendale17@gmail.com',
-        username: 'JaxParrow'
-      }
+      user
     });
-  // });
+  });
 };
+
+
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     email: DataTypes.STRING,
+//     username: DataTypes.STRING,
+//     password: DataTypes.STRING
+//   }, {
+//     classMethods: {
+//       associate: function(models) {
+//         // associations can be defined here
+//       }
+//     }
+//   });
+//   return User;
+// };
