0bc7c92a561f33d68d0416f03a8cd4e4368778f0
Glendale Acosta <gglendale17@gmail.com>
2018-04-28 22:28:29 -0700

    Added more video details

--- app-server/controllers/roomCtrl.js
+++ app-server/controllers/roomCtrl.js
@@ -4,18 +4,24 @@ const Room = require('../models/Room')(sequelize, DataTypes);
 const shortid = require('shortid');
 
 exports.postRoom = (req, res) => {
+
+  if (!req.body.roomId) {
   const id = shortid.generate();
-  Room
-    .upsert({id})
-    .then((created) => {
-      if (created) {
-        return res.json({
-          roomId: id,
-        })
-      } else {
-        return res.json({
-          error: 'shit'
-        })
-      }
-    });
+    Room
+      .upsert({id})
+      .then((created) => {
+        if (created) {
+          return res.json({
+            roomId: id,
+          })
+        } else {
+          return res.json({
+            error: 'shit'
+          })
+        }
+      });
+  } else if (req.body.roomId) {
+    console.log(`we are here. roomId: ${req.body.roomId}`);
+    // Room.find({where: })
+  }
 };
