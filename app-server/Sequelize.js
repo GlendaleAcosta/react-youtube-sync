9003c98bd5ec4d21fd26eae422022d59e60a7005
Glendale Acosta <gglendale17@gmail.com>
2018-05-22 16:02:03 -0700

    fixed connection with heroku db

--- app-server/Sequelize.js
+++ app-server/Sequelize.js
@@ -10,7 +10,7 @@ const Sequelize = require('sequelize');
 //     idle: 10000,
 //   },
 // });
-const sequelize = new Sequelize('23khdps6gdcrn host=ec2-50-19-224-165.compute-1.amazonaws.com', 'atugtkezijifrl', '02cbd4b91746016d3e2fe865abc8d2909f8def9df791a7463f182918ac60bd71', {
+const sequelize = new Sequelize('d23khdps6gdcrn', 'atugtkezijifrl', '02cbd4b91746016d3e2fe865abc8d2909f8def9df791a7463f182918ac60bd71', {
   host: 'ec2-50-19-224-165.compute-1.amazonaws.com',
   dialect: 'postgres',
   pool: {
